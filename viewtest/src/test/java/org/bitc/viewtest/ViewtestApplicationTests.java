285d1db597d0ca1221c51e05caebf9aefe90b1fe
ktwook5879 <ktwook5879@naver.com>
2023-12-13 11:09:36 +0900

    2023-12-13 슬라이드바 구현

---
+++ viewtest/src/test/java/org/bitc/viewtest/ViewtestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bitc.viewtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
