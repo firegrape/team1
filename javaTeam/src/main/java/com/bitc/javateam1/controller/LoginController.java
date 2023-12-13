b463657160d3be1092c417eb613aed71c2d13f4e
ktwook5879 <ktwook5879@naver.com>
2023-12-13 09:36:52 +0900

    2023-12-12 슬라이드바 구현

---
+++ javaTeam/src/main/java/com/bitc/javateam1/controller/LoginController.java
@@ -0,0 +1,4 @@
+package com.bitc.javateam1.controller;
+
+public class LoginController {
+}
---
+++ viewtest/src/main/java/org/bitc/viewtest/ViewtestApplication.java
@@ -0,0 +1,13 @@
+package org.bitc.viewtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewtestApplication.class, args);
+	}
+
+}
