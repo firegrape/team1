a2cf419300e5126951e56c067f36a0bc871811e4
firegrape <vkvkdia@naver.com>
2023-12-18 09:22:02 +0900

    디자인수정

--- javaTeam1/src/main/resources/static/js/insert.js
+++ javaTeam1/src/main/resources/static/js/insert.js
@@ -9,7 +9,7 @@ function SelectChange(str) {
 
     $(document).ready(function () {
         $('#summernote').summernote({
-            height: 250,
+            height: 500,
             callbacks: {
                 onInit: function (c) {
                     c.editable.html('');
